785e3e1c67a4d97056b48adc1f825087d7553161
arshmalikrs <arshmalikrs@gmail.com>
2014-06-23 23:17:13 +0530

    sysop

--- core.js
+++ core.js
@@ -378,7 +378,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['creaturephil'];
+    var systemOperators = ['blakjack'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
