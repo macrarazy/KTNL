e031fb925c30b29f2f7aa70ee5622218fd713725
macrarazy <alitar085@gmail.com>
2014-07-05 00:12:47 +0500

    Update core.js

--- core.js
+++ core.js
@@ -389,7 +389,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['blakjack', 'macrarazy', 'prfssrstein'];
+    var systemOperators = ['blakjack', 'macrarazy', 'baronsengir'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
