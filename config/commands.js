8b6ae67d4a44f65c04a6b7fb6f6955935d16d658
macrarazy <alitar085@gmail.com>
2014-07-07 04:20:44 +0500

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1296,6 +1296,14 @@ var commands = exports.commands = {
 			'<b>Catchphrase:</b> I may be stupid, but I\'m also dumb.<center>');
 	},
 	
+	shofu: function(target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox('<center><img src="http://static2.businessinsider.com/image/508807266bb3f70f5d000000/15-things-that-make-barack-obama-sad.jpg" height="476" width="357"><br />' +
+			'<img src="http://pbs.twimg.com/profile_images/1538004667/Document0002.png"><br />' +
+			'<b><blink>Ace: Darniggatan</blink></b><br />' +
+			'<b>The realest nigga you have ever seen</b>');
+	},
+	
 	sk: 'silverkill',
 	silverkill: function(target, room, user) {
 		if (!this.canBroadcast()) return;
