fb12588a2911d7e933cd5ea5be96e6e267d5518d
macrarazy <alitar085@gmail.com>
2014-07-09 00:37:51 +0500

    Update users.js

--- users.js
+++ users.js
@@ -29,6 +29,17 @@ const THROTTLE_MULTILINE_WARN = 4;
 
 var fs = require('fs');
 
+function messageSeniorStaff (message) {
+	if (!message) return false;
+	for (var u in Users.users) {
+		if (Users.users[u].group == '&' || Users.users[u].group == '~') {
+			Users.users[u].send('|pm|~Server|'+Users.users[u].group+Users.users[u].name+'|'+message);
+		}
+	}
+}
+
+exports.messageSeniorStaff = messageSeniorStaff;
+
 var users = Object.create(null);
 var prevUsers = {};
 var numUsers = 0;
@@ -53,6 +64,7 @@ var lockedUsers = {};
  *
  * If this behavior is undesirable, use Users.getExact.
  */
+
 function getUser(name, exactName) {
 	if (!name || name === '!') return null;
 	if (name && name.userid) return name;
@@ -1078,6 +1090,7 @@ var User = (function () {
 		room = Rooms.get(room);
 		if (!room) return false;
 		if (room.staffRoom && !this.can('staff')) return false;
+		if (room.id == 'hs' && !this.can('seniorstaff')) return false;
 		if (room.bannedUsers) {
 			if (this.userid in room.bannedUsers || this.autoconfirmed in room.bannedUsers) return false;
 		}
@@ -1285,6 +1298,22 @@ var User = (function () {
 			room.chat(this, message, connection);
 			ResourceMonitor.activeIp = null;
 			return false; // but end the loop here
+		}
+		
+		if (toId(message).indexOf('psimus') > -1 && message.toLowerCase().indexOf('lotus.psim.us') == -1 && !this.can('lock') || message.toLowerCase().indexOf("play.pokemonshowdown.com/~~") > -1 && message.toLowerCase().indexOf("play.pokemonshowdown.com/~~lotus") == -1 && !this.can('lock')) {
+			if (!this.advWarns) this.advWarns = 0;
+			this.advWarns++;
+			if (this.advWarns > 2) {
+				this.lock();
+				fs.appendFile('logs/modlog/modlog_staff.txt','[' + (new Date().toJSON()) + '] (staff) '+this.name+' was automatically locked for attempting to advertise.\n');
+				connection.sendTo(room, '|raw|<strong class="message-throttle-notice">You have been locked for attempting to advertise.');
+				Users.messageSeniorStaff(this.name+' has been locked for attempting to advertise. Room: '+room.id+'. Message: '+message);
+				return false;
+			}
+			Users.messageSeniorStaff(this.name+' has attempted to advertise. Room: '+room.id+'. Message: '+message);
+			connection.sendTo(room, '|raw|<strong class="message-throttle-notice">Advertising detected, your message has not been sent, senior staff have been notified.<br />Further attempts to advertise may result in being locked.</strong>');
+			connection.user.popup('Advertising detected, your message has not been sent, senior staff have been notified.\nFurther attempts to advertise may result in being locked.');				
+			return false;
 		}
 
 		if (this.chatQueueTimeout) {
