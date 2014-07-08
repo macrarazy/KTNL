de0ed895a316ed127e49d3db390c0a6c6cb88adf
macrarazy <alitar085@gmail.com>
2014-07-08 06:58:05 +0500

    Update components.js

--- components.js
+++ components.js
@@ -35,6 +35,8 @@ var components = exports.components = {
         this.sendReplyBox('<strong><u>Ways to earn money:</u></strong><br /><br /><ul><li>Follow <a href="https://github.com/CreaturePhil"><u><b>CreaturePhil</b></u></a> on Github for 5 bucks.</li><li>Star this <a href="https://github.com/CreaturePhil/Showdown-Boilerplate">repository</a> for 5 bucks. If you don\'t know how to star a repository, click <a href="http://i.imgur.com/0b9Mbff.png">here</a> to learn how.</li><li>Participate in and win tournaments.</li><br /><br />Once you done so pm an admin. If you don\'t have a Github account you can make on <a href="https://github.com/join"><b><u>here</b></u></a>.</ul>');
     },
 
+    staff: 'stafflist',
+    serverstaff: 'stafflist',
     stafflist: function (target, room, user) {
         var buffer = {
             admins: [],
