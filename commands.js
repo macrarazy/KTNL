73dc7ca2003a855fc956c7a4f6e1c57844fadb96
macrarazy <alitar085@gmail.com>
2014-07-08 21:35:54 +0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -1577,6 +1577,26 @@ var commands = exports.commands = {
 			process.exit();
 		}, 10000);
 	},
+	
+	restart: function(target, room, user) {
+		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever'); 
+		} catch(e) {
+			return this.sendReply('/restart requires the "forever" module.');
+		}
+
+		if (!Rooms.global.lockdown) {
+			return this.sendReply('For safety reasons, /restart can only be used during lockdown.');
+		}
+
+		if (CommandParser.updateServerLock) {
+			return this.sendReply('Wait for /updateserver to finish before using /restart.');
+		}
+		this.logModCommand(user.name + ' used /restart');
+		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
+	},
 
 	loadbanlist: function (target, room, user, connection) {
 		if (!this.can('hotpatch')) return false;
