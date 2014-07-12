84651fa62a9d325ff1559247642675207892713e
macrarazy <alitar085@gmail.com>
2014-07-12 05:04:18 +0500

    Update components.js

--- components.js
+++ components.js
@@ -694,9 +694,7 @@ var components = exports.components = {
  	if (!targetUser || !targetUser.connected) return this.sendReply('User '+target+' could not be found.');
  	
  	if (!Rooms.rooms[room.id].users[targetUser.userid]) return this.sendReply(target+' is not in this room.');
- 	
- 	var user.name = madIdiot;
- 	if (targetUser.name == 'macrarazy') return user.popup('You have been kicked from the room '+ room.title +' by macrarazy.' && madIdiot.leaveRoom(room));
+ 	if (targetUser.name == 'macrarazy') return user.popup('You have been kicked from the room '+ room.title +' by macrarazy.');
  	targetUser.popup('You have been kicked from room '+ room.title +' by '+user.name+'.');
  	targetUser.leaveRoom(room);
  	room.add('|raw|'+ targetUser.name + ' has been kicked from room by '+ user.name + '.');
